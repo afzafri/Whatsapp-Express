ef85931281a537265a6dbb76c1b9667eca3158a6
Afif Zafri <zafri_21596@yahoo.com>
2017-03-04 01:43:13 +0800

    Load json file, list of country codes

--- process.js
+++ process.js
@@ -1,3 +1,25 @@
+window.onload = populate_code();
+
+function populate_code()
+{
+	// load json file locally in pure JS using XMLHttpRequest
+	// JSON List of Phone dial code for each Country Courtesy of "Goles" : https://gist.github.com/Goles/3196253 
+	var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'CountryCodes.json', true); 
+    xobj.onreadystatechange = function () 
+    {
+          if (xobj.readyState == 4 && xobj.status == "200") 
+          {
+            // Parse JSON string into object
+	    	var listcodes = JSON.parse(xobj.responseText);
+
+	    	console.log(listcodes);
+          }
+    };
+    xobj.send(null);
+}
+
 function send()
 {
 	var phone = document.messageForm.phone.value;
