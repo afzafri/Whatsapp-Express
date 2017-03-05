bc57aeb96d133637f7a651a5136a8a4517fe1891
afzafri <zafri_21596@yahoo.com>
2017-03-05 17:03:49 +0800

    add form validation, determine if phone number is a valid numeric

--- process.js
+++ process.js
@@ -35,13 +35,22 @@ function send()
 	var phone = dialcode + document.messageForm.phone.value;
 	var message = encodeURI(document.messageForm.message.value); // need to encode the text
 	
-	window.location = "whatsapp://send?text="+message+"&phone="+phone;
-	/* 
-	this is Whatsapp's URL scheme
-	read: 
-	https://www.whatsapp.com/faq/en/general/26000030
-	https://www.whatsapp.com/faq/en/android/28000012
-	*/
+	// validation
+	if(!isNaN(phone))
+	{
+		window.location = "whatsapp://send?text="+message+"&phone="+phone;
+		/* 
+		this is Whatsapp's URL scheme
+		read: 
+		https://www.whatsapp.com/faq/en/general/26000030
+		https://www.whatsapp.com/faq/en/android/28000012
+		*/
+	}
+	else
+	{
+		alert("Please enter a valid phone number.");
+		document.messageForm.phone.focus();
+	}
 
 	return false;
 }
